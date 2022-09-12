06eb5cc9a568184e1af30e25bc8976fed802fbb5
Caezario666 <111202214608@mhs.dinus.ac.id>
2022-09-12 22:56:10 +0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Caezario Rafie Anggara Ziyadatullah \nA11.2022.14608 \nTeknik Informatika" << endl;
+    cout << "JL. Gunung Talang NO. 36, Gajahmungkur, Semarang" << endl;
+    return 0;
+}
